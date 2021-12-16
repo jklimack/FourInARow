dd692ad202cc4cae50407a332a3504bc7cbdd691
jklimack <56316767+jklimack@users.noreply.github.com>
2021-12-16 11:49:34 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -4,19 +4,18 @@ public class Runner {
     
     public static void main(String[] args) {
         Environment e = new ConnectFour();
-        Player[] players = new Player[2];
+        int numPlayers = 2;
+        Player[] players = new Player[numPlayers];
         players[0] = new BasePlayer(0);
-        //replace with
-        //players[0[= new Ariana(0);
         
         players[1] = new BasePlayer(1);
         
         int playerMove = 0;
-        int c = 0;
+        // int c = 0;
         
         while(!e.isGameOver()){
-            c = c+1;
-            if(c>5) break;
+            // c = c+1;
+            // if(c>5) break;
             
             players[playerMove].setGameState(e.getState());
             //give player a board
